55dbdbe6a10fb8fe9d0fdfdb6f58c579b7993d29
zzeeeemp <vladguk5443@gmail.com>
2024-04-22 17:12:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const carsFunctions = require('./cars');
+const htmlGenerator = require('./htmlGenerator');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    if (req.url === '/') {
+        const cars = carsFunctions.getCars();
+        
+        console.log(cars); // Wypisuje tablicę samochodów w konsoli
+
+        res.write(htmlGenerator.getHTMLDocumentStart());
+        res.write('<body>');
+        
+        // Wypisuje informacje o samochodach
+        cars.forEach(car => {
+            const carInfo = carsFunctions.getCarInformation(car.id);
+            res.write(`<p>${carInfo}</p>`);
+        });
+
+        // Wypisuje wiek samochodów
+        cars.forEach(car => {
+            const carAge = carsFunctions.getCarAge(car.id);
+            res.write(`<p>${carAge}</p>`);
+        });
+
+        res.write('</body>');
+        res.write(htmlGenerator.getHTMLDocumentEnd());
+        res.end();
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
